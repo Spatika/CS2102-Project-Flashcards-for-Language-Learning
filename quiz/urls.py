34bf9ce55fdd78dbddd5db655f628b938af80d7f
supraja2610 <suprajabhavanisekhar@gmail.com>
2015-04-04 20:11:44 +0800

    Back end of search done

--- quiz/urls.py
+++ quiz/urls.py
@@ -3,5 +3,7 @@ from quiz import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^user/$', views.userPage, name='user'),
     url(r'^search/$', views.search, name='search'),
+    # url(r'^search/(?P<user_id>[\w\s.]+)/$', views.search, name='search'),
 )
--- quiz/views.py
+++ quiz/views.py
@@ -10,18 +10,29 @@ def index(request):
 	context = RequestContext(request, {})
 	return HttpResponse(template.render(context))
 
-def search(request):
-	decode_json = json.loads(request.body)
-	print(decode_json)
-	# decode_json = 'German'
-	template = loader.get_template('quiz/index.html')
-	sets = Set.objects.filter(
-		Q(title__contains=decode_json)|
-		Q(description__contains=decode_json)|
-		Q(language_from__name__contains=decode_json)|
-		Q(language_to__name__contains=decode_json)
-		)
-	print(sets)
-	context = {'Sets':sets}
+def userPage(request):
+	template = loader.get_template('quiz/userPage.html')
+	context = RequestContext(request, {})
 	return HttpResponse(template.render(context))
-	
+
+# def search(request,user_id):
+def search(request):
+	template = loader.get_template('quiz/userPage.html')
+	if request.POST:
+		decode_json = request.POST.get('srch-term')
+		user_sets = Set.objects.filter(
+			Q(title__contains=decode_json)|
+			Q(description__contains=decode_json)|
+			Q(language_from__name__contains=decode_json)|
+			Q(language_to__name__contains=decode_json)
+			).filter(user__name='shweta')
+		print(user_sets)
+		other_sets = Set.objects.filter(
+			Q(title__contains=decode_json)|
+			Q(description__contains=decode_json)|
+			Q(language_from__name__contains=decode_json)|
+			Q(language_to__name__contains=decode_json)
+			).filter(~Q(user__name='shweta'))
+		print(other_sets)
+		context = {'UserSets':user_sets, 'OtherSets':other_sets}
+	return HttpResponse(template.render(context))
