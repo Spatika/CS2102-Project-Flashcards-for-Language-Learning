25a594ee5b2800b4d4590dbe87cfbb751040484e
Shweta Sharma <shweta.8448@gmail.com>
2015-04-04 19:23:49 +0800

    Added create set view

--- quiz/urls.py
+++ quiz/urls.py
@@ -3,4 +3,5 @@ from quiz import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^set/create/', views.create_set, name='set_create'),
 )
--- quiz/views.py
+++ quiz/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.shortcuts import get_object_or_404, render
+from .models import Card, Set, User, Language
+import json
 
 def index(request):
 	template = loader.get_template('quiz/index.html')
 	context = RequestContext(request, {})
 	return HttpResponse(template.render(context))
 
+def create_set(request):
+	data = json.loads(request.body)
+	user_set_data = data['set']
+	user_set = Set(user=User.objects.get(pk=user_set_data['user']), 
+		title=user_set_data['title'], 
+		description=user_set_data['description'], 
+		language_to=Language.objects.get(pk=user_set_data['language_to']), 
+		language_from=Language.objects.get(pk=user_set_data['language_from']))
+	
+	user_set.save()
+	user_set_cards = user_set_data['cards']
+	for card in user_set_cards:
+		user_set_card = Card(term=card['term'],
+			definition=card['definition'],
+			set=Set.objects.get(title=user_set_data['title'], 
+				user=User.objects.get(pk=user_set_data['user'])))
+		user_set_card.save()
+	return HttpResponse()
