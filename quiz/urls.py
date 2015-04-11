e93027da1bb92ce82f46ec8083b6882eba9da883
Edwin <24edwin@gmail.com>
2015-04-11 17:56:18 +0800

    deleteSet

--- quiz/urls.py
+++ quiz/urls.py
@@ -10,6 +10,8 @@ urlpatterns = patterns('',
 
     # url(r'^user/(?P<user_id>\d+)/$', views.users, name='account'),
     url(r'^search/$', views.search, name='search'),
+	url(r'^delete/(?P<set_id>\d+)/$', views.deleteSet, name='delete'),
+	
     # url(r'^search/(?P<user_name>[\w\s.])/$', views.search2, name='search'),
     url(r'^set/create/', views.set_create, name='set_create'),
 )
--- quiz/views.py
+++ quiz/views.py
@@ -86,6 +86,28 @@ def search(request):
 		print(other_sets)
 		context = {'sets': sets, 'number_of_sets': len(sets), 'OtherSets':other_sets, 'number_of_others': len(other_sets), 'username': request.user}
 	return HttpResponse(template.render(context))
+	
+def deleteSet(request,set_id):
+	
+	template = loader.get_template('quiz/dashboard.html')
+	if request.POST:
+		decode_json = request.POST.get('srch-term')
+		sets = Set.objects.filter(
+			Q(title__contains=decode_json)|
+			Q(description__contains=decode_json)|
+			Q(language_from__name__contains=decode_json)|
+			Q(language_to__name__contains=decode_json)
+			).filter(user__username=request.user)
+		print(sets)
+		other_sets = Set.objects.filter(
+			Q(title__contains=decode_json)|
+			Q(description__contains=decode_json)|
+			Q(language_from__name__contains=decode_json)|
+			Q(language_to__name__contains=decode_json)
+			).filter(~Q(user__username=request.user))
+		print(other_sets)
+		context = {'sets': sets, 'number_of_sets': len(sets), 'OtherSets':other_sets, 'number_of_others': len(other_sets), 'username': request.user}
+	return HttpResponse(template.render(context))
 
 def set_create(request):
 	data = json.loads(request.body)
