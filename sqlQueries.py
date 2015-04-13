2281b1b4fbb81ebf5a928fe51f684954de8644eb
Spatika Narayanan <spatika.n@gmail.com>
2015-04-13 08:55:45 +0800

    Updated sqlQueries.py

--- sqlQueries.py
+++ sqlQueries.py
@@ -34,6 +34,30 @@ def add_other_user_set (setTitle, setUser, currUser):
 	queryString = "INSERT INTO Adds VALUES(%s, %s, %s)"
 	return c.execute(queryString % (currUser, setTitle, setUser))
 
-def delete_Set(setTitle, setUser);
+def delete_Set(setTitle, setUser):
 	queryString = "DELETE​​ FROM ​​Deck_owned_by_User ​deck WHERE​ deck​.user=​'%s' AND​​ deck​.​title​='%s'"
 	return c.execute(queryString % (setUser, setTitle))	 
+
+def login_user (username, password):
+	queryString = "SELECT * FROM User WHERE email = %s AND password = %s"
+	return c.execute(queryString % (username, password)).fetch()
+
+def signup_user(username, password):
+	queryString = "INSERT INTO User VALUES(%s, %s, %s)"  
+	return c.execute(queryString % (username, username, password))
+
+def create_card(username, setTitle, term, defintion):
+	queryString = "INSERT INTO Card_in_Deck VALUES(%s, %s, %s, %s)"
+	return c.execute(queryString % (usernamem setTitle, term, definition))
+
+# edit_set
+
+def edit_set(field, newValue, setTitle):
+	queryString = "UPDATE Deck_owned_by_User SET '%s' = '%s' WHERE title = '%s'"
+	return c.execute(queryString % (field, newValue, setTitle))
+
+	
+
+
+
+
